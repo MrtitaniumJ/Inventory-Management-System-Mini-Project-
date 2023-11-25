57e2649e60c701691a7f3912ca2ab3089ea0ea8d
Yatharth2609 <yatharth.mishra2002@gmail.com>
2023-11-25 19:27:05 +0530

    Forms

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Settings from "./pages/Settings";
 import Notifications from "./pages/Notifications";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
-import Logout from "./pages/Logout";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 
@@ -19,15 +19,12 @@ function App() {
   return (
     <div className="bg-gray-200 h-screen text-base">
       <BrowserRouter>
-        <Header >
-          <Routes>
-          </Routes>
-        </Header>
+        <Header />
         <Sidebar>
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />}/>
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/sign-in" element={<Login />}/>
             <Route path="/inventoryMgmt" element={<InventoryMgmt />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/orders" element={<Orders />} />
@@ -37,7 +34,7 @@ function App() {
             <Route path="/notifications" element={<Notifications />} />
             <Route path="/search" element={<Search />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/logout" element={<Logout />} />
+            <Route path="/sign-up" element={<SignUp />} />
           </Routes>
         </Sidebar>
       </BrowserRouter>
